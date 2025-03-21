a9f4246fef99a604d283ae01ffa876549eea7be8
Malin Haugsdal <33.haugsdal@gmail.com>
2025-03-22 00:11:41 +0100

    La til filer for oppsett

---
+++ src/main/java/oblig2_app/OBJ_Arbeidskrav/ObjArbeidskravApplication.java
@@ -0,0 +1,13 @@
+package oblig2_app.OBJ_Arbeidskrav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjArbeidskravApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjArbeidskravApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oblig2_app/OBJ_Arbeidskrav/ObjArbeidskravApplicationTests.java
@@ -0,0 +1,13 @@
+package oblig2_app.OBJ_Arbeidskrav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjArbeidskravApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
